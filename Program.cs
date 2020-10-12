2c29273175ebba53a5a8a8e1c4bdc82395eb18df
Emilia <emiartola@gmail.com>
2020-10-11 23:58:17 -0300

    Separación en clases y más

--- Program.cs
+++ Program.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace Sudoku
 {
     class Program
     {
+        static int fila = 0, columna = 0;
+
         static void Main(string[] args)
-        {
-            int [,] matriz = new int[9,9];
+        {          
 
-            int fila = 0, columna = 0;
+            var rnd = new Random();
+            int index = rnd.Next(Juegos.juegos.Count);
 
-            CrearMatriz(matriz);
-            MostrarMatriz(matriz,columna,fila);
+            Tablero tablero = new Tablero(Juegos.juegos[index]);
 
-            while (true)
+            tablero.MostrarMatriz(columna, fila);
+
+            while (tablero.TableroIncompleto)
             {
                 ConsoleKeyInfo key = Console.ReadKey();
                 switch (key.Key)
@@ -34,66 +38,16 @@ namespace Sudoku
                 }
 
                 int value;
-                if (int.TryParse(""+key.KeyChar,out value))
+                if (int.TryParse("" + key.KeyChar, out value))
                 {
-                    try
-                    {
-                        matriz[fila, columna] = value;
-                    }
-                    catch (Exception e)
-                    {
-
-                    }
+                    tablero.SetCelda(value, fila, columna);
+                    
                 }
 
-                MostrarMatriz(matriz, columna, fila);
+                tablero.MostrarMatriz( columna, fila);
             }
 
         }
 
-
-        private static List<string> juegos = new List<string>(){
-            "023500089008910350049000060000089675095020800386700000900060024007000916204097030",
-            "015004000002700006006002941024008000700421005500309070008900513497030008103086794",
-            "901005002000900006430608519509076430308500020000380975150060000780104000000800167" };
-
-
-        private static void CrearMatriz(int[,] matriz)
-        {
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    matriz[i , j ] = int.Parse(""+juegos[0][j+i*9]);
-                }
-            }
-            
-        }
-        private static void MostrarMatriz(int[,] matriz,int columna, int fila)
-        {
-            //Console.Clear();
-            Console.SetCursorPosition(1,0);
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine(" - "+" S U D O K U     G A M E "+ " - ");
-            Console.WriteLine();
-
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    if (fila == i && columna == j)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Blue; 
-                    }else
-                    {
-                        Console.ForegroundColor = ConsoleColor.White;
-                    }
-                    Console.Write(/*(fila==i&&columna==j ? "-" : " ")*/" " + (matriz[i, j]==0?"-":matriz[i,j].ToString())+((j+1)%3==0?" |":" "));
-
-                }
-                Console.WriteLine();
-                Console.WriteLine(((i + 1) % 3 == 0 ? "---------+---------+---------" : ""));
-            }
-        }
     }
 }
---
+++ Tablero.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Sudoku
+{
+    
+    class Tablero
+    {
+        private string juego;
+        private int[,] matrizOriginal;
+        private int[,] matriz;
+
+        private bool tableroIncompleto = true;
+        public bool TableroIncompleto
+        {
+            get { 
+                return tableroIncompleto; 
+            }
+        }
+
+        public Tablero(string juego)
+        {
+            this.juego = juego;
+
+            CrearMatriz();
+        }
+
+        public void CrearMatriz()
+        {
+            matrizOriginal = new int[9, 9];
+            matriz = new int[9, 9];
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    matrizOriginal[i, j] = int.Parse("" + juego[j + i * 9]);
+                    matriz[i,j] = matrizOriginal[i, j];
+                }
+            }
+
+
+        }
+        public void MostrarMatriz(int columna, int fila)
+        {
+            //Console.Clear();
+            Console.SetCursorPosition(1, 0);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" - " + " S U D O K U     G A M E " + " - ");
+            Console.WriteLine();
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (fila == i && columna == j)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                    }
+                    else if (IsCeldaValida(i,j))
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+
+                    Console.Write(/*(fila==i&&columna==j ? "-" : " ")*/" " + (matriz[i, j] == 0 ? "-" : matriz[i, j].ToString()) + ((j + 1) % 3 == 0 ? " |" : " "));
+
+                }
+                Console.WriteLine();
+                Console.WriteLine(((i + 1) % 3 == 0 ? "---------+---------+---------" : ""));
+            }
+        }
+
+        public void SetCelda(int value, int fila, int columna)
+        {
+            //Habría que cambiar el try por varios if, para limitar lo que se ingresa,etc 
+            try
+            {
+                if (matrizOriginal[fila, columna] == 0)
+                {
+                    matriz[fila, columna] = value;
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        private bool IsCeldaValida(int fila, int columna)
+        {
+            if (matriz[fila, columna] != 0)
+            {
+                //fila invalida (valor fila no se repite)
+                for (int i = 0; i < 9; i++)
+                {
+                    if (
+                        i != columna
+                        &&
+                        matriz[fila, i] ==
+                        matriz[fila, columna])
+                    {
+                        return false;
+                    }
+
+                }
+                //columna invailda (valor columna no se repite)
+                for (int i = 0; i < 9; i++)
+                {
+                    if (
+                        i != fila
+                        &&
+                        matriz[i,columna] ==
+                        matriz[fila, columna])
+                    {
+                        return false;
+                    }
+
+                }
+
+                //grupo invalido (valor dentro del 3x3 no se repite) 
+
+            }
+            return true;
+        }
+       
+
+    }
+}
+
