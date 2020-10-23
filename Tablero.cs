f2afc74ade2feb3736dc3435ef18bb2c111e6646
Emilia <emiartola@gmail.com>
2020-10-23 11:42:45 -0300

    Actualizado

--- Tablero.cs
+++ Tablero.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Sudoku
 {
-    
+
     class Tablero
     {
         private string juego;
@@ -15,8 +15,9 @@ namespace Sudoku
         private bool tableroIncompleto = true;
         public bool TableroIncompleto
         {
-            get { 
-                return tableroIncompleto; 
+            get
+            {
+                return tableroIncompleto;
             }
         }
 
@@ -36,7 +37,7 @@ namespace Sudoku
                 for (int j = 0; j < 9; j++)
                 {
                     matrizOriginal[i, j] = int.Parse("" + juego[j + i * 9]);
-                    matriz[i,j] = matrizOriginal[i, j];
+                    matriz[i, j] = matrizOriginal[i, j];
                 }
             }
 
@@ -46,7 +47,7 @@ namespace Sudoku
         {
             //Console.Clear();
             Console.SetCursorPosition(1, 0);
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine(" - " + " S U D O K U     G A M E " + " - ");
             Console.WriteLine();
 
@@ -58,16 +59,26 @@ namespace Sudoku
                     {
                         Console.ForegroundColor = ConsoleColor.Blue;
                     }
-                    else if (IsCeldaValida(i,j))
+                    else if (IsCeldaValida(i, j))
                     {
                         Console.ForegroundColor = ConsoleColor.White;
                     }
                     else
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
+                        if (matriz[i, j] != matrizOriginal[i, j])
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
                     }
 
-                    Console.Write(/*(fila==i&&columna==j ? "-" : " ")*/" " + (matriz[i, j] == 0 ? "-" : matriz[i, j].ToString()) + ((j + 1) % 3 == 0 ? " |" : " "));
+                    Console.Write(/*(fila==i&&columna==j ? "-" : " ")*/" " + (matriz[i, j] == 0 ? "-" : matriz[i, j].ToString()));
+
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(((j + 1) % 3 == 0 ? " |" : " "));
 
                 }
                 Console.WriteLine();
@@ -77,9 +88,10 @@ namespace Sudoku
 
         public void SetCelda(int value, int fila, int columna)
         {
-            //Habría que cambiar el try por varios if, para limitar lo que se ingresa,etc 
+
             try
             {
+                //para no poder modificar los valores del tablero inicial.
                 if (matrizOriginal[fila, columna] == 0)
                 {
                     matriz[fila, columna] = value;
@@ -95,7 +107,7 @@ namespace Sudoku
         {
             if (matriz[fila, columna] != 0)
             {
-                //fila invalida (valor fila no se repite)
+                //fila invalida (valor no se repite en fila)
                 for (int i = 0; i < 9; i++)
                 {
                     if (
@@ -108,13 +120,13 @@ namespace Sudoku
                     }
 
                 }
-                //columna invailda (valor columna no se repite)
+                //columna invalida (valor no se repite en columna)
                 for (int i = 0; i < 9; i++)
                 {
                     if (
                         i != fila
                         &&
-                        matriz[i,columna] ==
+                        matriz[i, columna] ==
                         matriz[fila, columna])
                     {
                         return false;
@@ -122,13 +134,36 @@ namespace Sudoku
 
                 }
 
-                //grupo invalido (valor dentro del 3x3 no se repite) 
 
+                //grupo invalido (valor dentro del 3x3 no se repite) 
+                int filaGrupo = (fila / 3);
+                int columnaGrupo = (columna / 3);
+                for (int i = 3 * filaGrupo; i < (filaGrupo + 1) * 3; i++)
+                {
+                    for (int j = 3 * columnaGrupo; j < (columnaGrupo + 1) * 3; j++)
+                    {
+
+                        if (fila != i && columna != j)
+                        {
+
+                            if (matriz[i, j] == matriz[fila, columna])
+                            {
+                                return false;
+                            }
+                        }
+                    }
+
+
+
+                }
             }
+
             return true;
         }
-       
+
 
     }
 }
+
+
 
